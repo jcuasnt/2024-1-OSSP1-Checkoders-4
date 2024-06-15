10399da451c4e1dc93e7096164a71c4621366d68
Yunsu0407 <129031570+Yunsu0407@users.noreply.github.com>
2024-06-15 19:50:16 +0900

    Delete WebPages directory

--- WebPages/log_in_page/script.jsx
+++
@@ -1,7 +0,0 @@
-function redirectToPage() {
-  // 이동할 페이지의 URL을 설정
-  var pageUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&prompt=login";
-  
-  // 페이지로 이동
-  window.location.href = pageUrl;
-}
